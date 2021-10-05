3231ada859c5a12057926c652c682771c6b5e389
gamzerol <erol.gmze@gmail.com>
2021-10-05 21:53:00 +0300

    completed

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 class Tour extends React.Component {
   render() {
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Tour from './Tour';
 import data from './data';
 
-const [tours, setInfo] = useState(data);
 class Tours extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tours: data
+    }
+  }
   render() {
     return(
-      tours.map(tour => {
-        <Tour image={tour.image} name={tour.name} price={tour.price} info={tour.info}/>
+      this.state.tours.map(tour => {
+        return(
+          <Tour image={tour.image} name={tour.name} price={tour.price} info={tour.info}/>
+        )
       })
     )
   }
